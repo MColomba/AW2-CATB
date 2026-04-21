d876c11ffad1e4a8a44a1cc17b62d624ba78a369
MColomba <matiascolomba@gmail.com>
2026-04-20 22:12:19 -0300

    Se corrigio la indentacion con alt shift f

--- Clase-200426/index.mjs
+++ Clase-200426/index.mjs
@@ -2,14 +2,14 @@ import http from 'node:http'
 import fsp from 'node:fs/promises'
 import path from 'node:path'
 
-try{
-    const server = http.createServer(async(request, response) => {
-        if(request.method === 'GET'){
-            if(request.url === '/usuarios'){
+try {
+    const server = http.createServer(async (request, response) => {
+        if (request.method === 'GET') {
+            if (request.url === '/usuarios') {
                 //1-Fetch
                 const respuesta = await fetch('https://api.escuelajs.co/api/v1/users')
                 const usuarios = await respuesta.json();
-                
+
                 //2-Escribir JSON
                 const ruta = path.resolve('usuarios.json')
                 const datosJson = JSON.stringify(usuarios, null, 4)
@@ -17,7 +17,7 @@ try{
 
                 //3-Leer JSON
                 const usuariosLocal = await fsp.readFile(ruta, 'utf-8')
-                
+
                 //4-Envio al cliente
                 response.method = 200
                 return response.end(usuariosLocal)
@@ -32,8 +32,8 @@ try{
         response.end('Recurso no encontrado')
     })
     server.listen(3000, () => {
-    console.log('Server running in http://localhost:3000')
+        console.log('Server running in http://localhost:3000')
     });
-}catch(error){
+} catch (error) {
     console.log('Error')
 }
